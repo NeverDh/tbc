263c56bad7b138298fb1736854231b8d764c02fb
NeverDh <63487149+NeverDh@users.noreply.github.com>
2023-09-21 15:21:44 -0300

    Final http

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = require("./routers/fluigRouter");
+const fluigRouter = require("./routers/fluigRouter");
 
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use(fluigRouter);
 
 module.exports = app;
